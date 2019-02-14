8798e93f3377461803475394c5382ef47cd6b758
严墨 <yanmo@yanmodeMacBook-Pro.local>
2019-02-14 12:57:00 +0800

    文档修改、测试用例构写

--- README.md
+++ README.md
@@ -1,26 +1,81 @@
-# XXX(组件名)
+# deepCopy
 
 ## 概述
 
-...
-
-## Install(安装)
-
-npm install --save ...
+>这是一个进行深拷贝的js插件
 
 ## Usage(使用)
 
-...
+发布成功后就可以使用了。使用有两种形式，一种是 `npm` 安装，一种是 `<script>` 引用。
 
-... 其他内容 ...
+### npm 安装：
 
-## Donation
+``` shell
+npm install --save deep-copy.js
+```
 
-...
+``` JavaScript
+import deepCopy from 'deep-copy.js'
+let a = [1, 2, 3]
+let b = deepCopy(a)
+b[0] = 4
+console.log(a, b); // [1, 2, 3] [4, 2, 3]
+// 或者
+const deepCopy = require('deep-copy.js')
+let a = [1, 2, 3]
+let b = deepCopy(a)
+b[0] = 4
+console.log(a, b); // [1, 2, 3] [4, 2, 3]
+```
+
+### `<script>`使用
+
+``` HTML
+<script src="//unpkg.com/deep-copy.js@1.0.1/lib/index.js"></script>
+<script>
+  console.log(window.deepCopy);
+  let a = [1, 2, 3]
+  let b = deepCopy(a)
+  b[0] = 4
+  console.log(a, b); // [1, 2, 3] [4, 2, 3]
+</script>
+```
+
+## 支持深拷贝的数据类型
+
+Number, String, Boolean, Array, Object, Node, Date
+
+## run(运行)
+
+``` bash
+# git clone ...
+git clone https://github.com/nongshuqiner/deep-copy.js.git
+
+# enter
+cd deep-copy.js
+
+# install dependencies
+npm install
+
+# open examples HTML
+npm run examples
+
+# 运行此命令将所有代码从 src 目录编译到 lib
+npm run build
+
+# 测试
+npm run test
+```
+
+## Donation(打赏)
+
+![payment-code.png](https://upload-images.jianshu.io/upload_images/4645892-20338f9a0a443ff2.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
 
 ## Contact me(联系我)
 
-...
+Just Contact Me At:
+- Email: ym1185509297@163.com
+- 简书: [言墨儿](https://www.jianshu.com/u/319464da1cc1)
 
 ## License
 
--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
-// import { expect } from 'chai' // 断言库 chai.js
-// import deepCopy from '../src/deep-copy.js'
-var expect = require('chai').expect
+// 断言库 chai.js
+var expect = require('chai').expect;
 var deepCopy = require('../src/index');
 
 // 测试脚本里面应该包括一个或多个describe块，称为测试套件（test suite）
@@ -8,18 +7,82 @@ describe('数组深拷贝', function () {
   // 每个describe块应该包括一个或多个it块，称为测试用例（test case）
   it('一维数组', () => {
     // 断言
-    let a = [1, 2, 3]
-    let b = deepCopy(a)
-    b[0] = 4
-    expect(b[0]).to.equal(4)
-    expect(a[0]).to.equal(1)
-  })
+    let a = [1, 2, 3];
+    let b = deepCopy(a);
+    b[0] = 4;
+    expect(b[0]).to.equal(4);
+    expect(a[0]).to.equal(1);
+  });
   it('数组子项为对象', () => {
     // 断言
-    let a = [{name: 'lina'}, {name: 'luxi'}, {name: 'nigulas'}]
-    let b = deepCopy(a)
-    b[0].name = 'saliya'
-    expect(b[0].name).to.equal('saliya')
-    expect(a[0].name).to.equal('lina')
-  })
-})
+    let a = [{name: 'lina'}, {name: 'luxi'}, {name: 'nigulas'}];
+    let b = deepCopy(a);
+    b[0].name = 'saliya';
+    expect(b[0].name).to.equal('saliya');
+    expect(a[0].name).to.equal('lina');
+  });
+});
+
+describe('对象深拷贝', function () {
+  it('基本对象', () => {
+    // 断言
+    let a = {
+      name: 'zhangsan',
+      age: 18
+    };
+    let b = deepCopy(a);
+    b.name = 'lisi';
+    b.age = 24;
+    expect(a.name).to.equal('zhangsan');
+    expect(a.age).to.equal(18);
+    expect(b.name).to.equal('lisi');
+    expect(b.age).to.equal(24);
+  });
+  it('对象嵌套', () => {
+    // 断言
+    let obj1 = { a: 0 , b: { c: 0}};
+    let obj2 = deepCopy(obj1);
+    obj1.a = 1;
+    obj2.a = 2;
+    obj2.b.c = 3;
+    expect(obj1.a).to.equal(1);
+    expect(obj1.b.c).to.equal(0);
+    expect(obj2.a).to.equal(2);
+    expect(obj2.b.c).to.equal(3);
+  });
+});
+
+describe('String深拷贝', function () {
+  it('String深拷贝前后内容一致', () => {
+    let a = 'abc';
+    let b = deepCopy(a);
+    expect(b).to.equal('abc');
+  });
+});
+
+describe('Number深拷贝', function () {
+  it('Number深拷贝前后内容一致', () => {
+    let a = 1024;
+    let b = deepCopy(a);
+    expect(b).to.equal(1024);
+  });
+});
+
+describe('Boolean深拷贝', function () {
+  it('Boolean深拷贝前后内容一致', () => {
+    let a = true;
+    let b = deepCopy(a);
+    expect(b.toString()).to.equal('true');
+  });
+});
+
+describe('Date深拷贝', function () {
+  let a = new Date();
+  let b = deepCopy(a);
+  it('Date深拷贝前后内容一致', () => {
+    expect(a.toString() === b.toString()).to.equal(true);
+  });
+  it('Date深拷贝内存不一致', () => {
+    expect(a === b).to.equal(false);
+  });
+});
